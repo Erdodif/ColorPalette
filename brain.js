9e22fcba6c66bc1af5082b0ba2c5a726030d5013
Erdodif <erdodif@gmail.com>
2021-09-14 19:13:27 +0200

    Generáltatás megvalósítása a felesleges ismétlések miatt

--- brain.js
+++ brain.js
@@ -3,8 +3,25 @@ elemek = ["primary","primary-variant",
         "background","surface","error",
         "on-primary","on-secondary",
         "on-background","on-surface","on-error"];
+nevek = ["Elsődleges", "Elsődleges Variáns",
+        "Másodlagos", "Másodlagos Variáns",
+        "Háttér", "Előtér", "Hiba",
+        "Elsődleges Betű", "Másodlagos Betű",
+        "Háttér Betű", "Előtér Betű", "Hiba Betű"]
 stilus = getComputedStyle(document.documentElement)
 function inicial(){
+    szinek_tartalom = "";
+    for (i = 0; i < elemek.length; i++)
+    {
+        szinek_tartalom += `
+            <div id="${elemek[i]}">
+                <label for="${elemek[i]}-i">
+                    ${nevek[i]}
+                </label>
+                <input id="${elemek[i]}-i" type="color">
+            </div>`
+    }
+    document.getElementById("szinek").innerHTML = szinek_tartalom;
     for (let i of elemek){
         let a = stilus.getPropertyValue('--s-'+i).replace(' ','');
         document.getElementById(i+"-i").value = a;
